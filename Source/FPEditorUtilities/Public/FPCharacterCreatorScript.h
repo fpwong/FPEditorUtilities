745ea13521aeb6895b890acc65e769521132af45
fpwong <func.pwong@gmail.com>
2021-07-01 20:55:37 +1000

    Initial commit

---
+++ Source/FPEditorUtilities/Public/FPCharacterCreatorScript.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/Skeleton.h"
+#include "Blutility/Classes/EditorUtilityObject.h"
+#include "Engine/EngineTypes.h"
+#include "FPCharacterCreatorScript.generated.h"
+
+class UAnimBlueprint;
+class UAnimInstance;
+class FAssetToolsModule;
+/**
+ *
+ */
+UCLASS(Blueprintable)
+class FPEDITORUTILITIES_API UFPCharacterCreatorScript : public UEditorUtilityObject
+{
+	GENERATED_BODY()
+
+	UFPCharacterCreatorScript();
+
+	UPROPERTY(EditAnywhere, meta = (ContentDir, LongPackageName))
+	FDirectoryPath DefaultPath;
+
+	UPROPERTY(EditAnywhere)
+	FString CharacterName;
+
+	UPROPERTY(EditAnywhere)
+	USkeletalMesh* CharacterSkeleton;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> CharacterClass;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAnimInstance> AnimInstanceClass;
+
+	UPROPERTY(EditAnywhere)
+	TArray<FString> MontagesToCreate;
+
+	UFUNCTION(BlueprintCallable)
+	void CreateCharacter();
+
+	UFUNCTION(BlueprintCallable)
+	void TryLoadObj();
+
+private:
+	FString DestinationFolder;
+
+	UAnimBlueprint* CreatedAnimBlueprint;
+
+	bool SelectDestinationFolder();
+
+	void CreateMontage(FAssetToolsModule& AssetToolsModule, FString Suffix);
+	void CreateCharacter(FAssetToolsModule& AssetToolsModule);
+	void CreateAnimBP(FAssetToolsModule& AssetToolsModule);
+	void CreateBlendspace(FAssetToolsModule& AssetToolsModule);
+
+	void SaveAsset(UObject* CreatedAsset);
+
+	void EditCharacterBlueprint(UObject* Object);
+};
